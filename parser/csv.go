eecb7b4d1c1a5bd0f02421db0ae0d988c26bb863
Yuuki Toriyama <github@toriyama.dev>
2024-05-25 13:20:05 +0900

    add: `parser/csv.go`

---
+++ parser/csv.go
@@ -0,0 +1,29 @@
+package parser
+
+import "strings"
+
+type Row = map[string]string
+
+func parse(csv string) (result []Row) {
+	line := strings.Split(csv, "\n")
+	if len(line) <= 1 {
+		print(result)
+		return
+	}
+
+	header := strings.Split(line[0], ",")
+	colsCount := len(header)
+
+	for i := 1; i < len(line); i++ {
+		row := strings.Split(line[i], ",")
+		if len(row) != colsCount {
+			// TODO: ヘッダー行の要素の個数と今見ている行の要素の個数が合わない場合エラーを投げる
+		}
+		parsed := make(Row, colsCount)
+		for j, elem := range row {
+			parsed[header[j]] = elem
+		}
+		result = append(result, parsed)
+	}
+	return
+}
---
+++ parser/csv_test.go
@@ -0,0 +1,38 @@
+package parser
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parse(t *testing.T) {
+	type args struct {
+		csv string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantResult []Row
+	}{
+		{
+			name:       "3列1行",
+			args:       args{"id,name,mail_address"},
+			wantResult: nil,
+		},
+		{
+			name: "3列2行",
+			args: args{"id,name,mail_address\n1,toriyama,toriyama@example.com\n2,hogehoge,hogehoge@example.com"},
+			wantResult: []Row{
+				{"id": "1", "name": "toriyama", "mail_address": "toriyama@example.com"},
+				{"id": "2", "name": "hogehoge", "mail_address": "hogehoge@example.com"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotResult := parse(tt.args.csv); !reflect.DeepEqual(gotResult, tt.wantResult) {
+				t.Errorf("parse() = %v, want %v", gotResult, tt.wantResult)
+			}
+		})
+	}
+}
