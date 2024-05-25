cbdbef8fbf575fc6f905d5fe689b5d7be98938f3
Yuuki Toriyama <github@toriyama.dev>
2024-05-26 01:31:25 +0900

    update: JavaScriptから呼び出せる`csv2json()`を作成
    JavaScriptとGoの間で文字列をやりとりする方法については以下を参考にした
    
    https://github.com/kajikentaro/wasm-go-tinygo-sample

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
-import "strconv"
+import (
+	"github.com/YuukiToriyama/gotiny-wasm-sandbox/parser"
+	"github.com/YuukiToriyama/gotiny-wasm-sandbox/serializer"
+	"unsafe"
+)
 
 func main() {}
 
-//export console_log
-func log(message string)
+//export csv2json
+func csvToJson(csv string) uint32 {
+	rows := parser.Parse(csv)
+	jsonStr := serializer.ToJson(rows)
 
-//export alert
-func alert(message string)
+	return stringToPtr(jsonStr)
+}
 
-//export multiply
-func multiply(left, right int) int {
-	log("left: " + strconv.Itoa(left) + "\n" + "right: " + strconv.Itoa(right))
-	alert("掛け算を実行します！")
-	return left * right
+// NOTICE: 以下は https://github.com/kajikentaro/wasm-go-tinygo-sample を参考にした。
+
+var wasmBuf [1024]byte
+
+//export getWasmBuffer
+func getBuffer() *byte {
+	return &wasmBuf[0]
+}
+
+var stringBufSize uint32
+
+//export getBufSize
+func getBufSize() uint32 {
+	return stringBufSize
+}
+
+func stringToPtr(s string) uint32 {
+	buf := []byte(s)
+	ptr := &buf[0]
+	unsafePtr := uintptr(unsafe.Pointer(ptr))
+
+	stringBufSize = uint32(len(buf))
+	return uint32(unsafePtr)
 }
